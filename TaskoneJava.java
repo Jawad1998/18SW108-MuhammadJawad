efe128c510cfe9c8620b925fe1bcfcd7307654c5
Jawad1998 <49369955+Jawad1998@users.noreply.github.com>
2019-04-07 23:30:04 +0500

    Add files via upload

---
+++ TaskoneJava.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taskone.java;
+
+/**
+ *
+ * @author HP
+ */
+public class TaskoneJava {
+
+    public static void main(String[] args) {
+        String name = "Jawad";
+char grade = 'A'; 
+float percentage = 85.00f;
+int totalmarks = 500;
+boolean status = true;
+System.out.println(name);
+System.out.println(grade);
+System.out.println(percentage);
+System.out.println(totalmarks);
+if(status==true) { 
+System.out.println("Pass");
+   }
+else {
+System.out.println("Fail");   }
+    }
+    
+}
---
+++ TasktwoJava.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasktwo.java;
+
+/**
+ *
+ * @author HP
+ */
+public class TasktwoJava {
+
+    public static void main(String[] args) {
+        double d=50.25;
+        int a =(int)d;
+        double f = d-a;
+        System.out.println("Actual Number:"+d);
+        System.out.println("Integral part:"+a);
+        System.out.println("Fractional Part:"+f);
+    }
+    
+}
